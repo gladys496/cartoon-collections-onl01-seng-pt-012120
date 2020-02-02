b0613e34fd41878a7f45f1472cd655862da02a87
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-02 07:32:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,4 +1,8 @@
-def 
+def def roll_call_dwarves(dwarves)
+  dwarves.each_with_index do |name, index|
+    index += 1
+    puts "#{index}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
